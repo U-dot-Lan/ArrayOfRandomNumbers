2de61eb1582377c74bc277917d3facf3ddef6269
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 20:33:29 +0300

    Добавьте файлы проекта.

---
+++ ArrayValues.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ArrayOfRandomNumbers
+{
+    public static class ArrayValues
+    {
+        public static void FillArray(int[] n)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < n.Length; i++)
+            {
+                int value = rand.Next(1, 6);
+                n[i] = value;
+            }
+        }
+
+        public static  void PrintIntersectValues(int[] n1, int[] n2)
+        {
+            int sum = 0;
+            for (int i = 0; i < n1.Length; i++)
+            {
+                for (int j = 0; j < n2.Length; j++)
+                {
+                    if (n1[i] == n2[j])
+                    {
+                        Console.WriteLine($"value: {n1[i]}, indexes: [{i} | {j}]");
+                        sum += n1[i];
+                    }
+                }
+            }
+            Console.WriteLine("sum of ALL intersect values {0}", sum);
+        }
+
+        public static void PrintSumIntersectValues(int[] n1, int[] n2)
+        {
+            var values = n1.Intersect(n2);
+            foreach(var i in values)
+            {
+                Console.WriteLine($"Intersect values: {i}");
+            }
+            int sum = n1.Intersect(n2).Sum();
+            Console.WriteLine($"sum of intersect values: {sum}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayOfRandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[10];
+            int[] nums2 = new int[10];
+
+            ArrayValues.FillArray(nums1);
+            ArrayValues.FillArray(nums2);
+            ArrayValues.PrintIntersectValues(nums1, nums2);
+            ArrayValues.PrintSumIntersectValues(nums1, nums2);
+        }
+
+        
+    }
+}
